79fec17bce10ca99dd1a4a99a793738a27eb3f29
imhefizh <hafidzsml@gmail.com>
2025-01-30 23:55:14 -0500

    step 5 done

--- src/Components/CartSlice.jsx
+++ src/Components/CartSlice.jsx
@@ -16,7 +16,35 @@ const CartSlice = createSlice({
                 state.cartItems.push({ ...action.payload, quantity: 1 });
             }
         },
+        removeItemFromCart(state, action) {
+            state.cartItems = state.cartItems.filter(item => item.id !== action.payload);
+        },
+        clearCart(state) {
+            state.cartItems = [];
+        },
+        increaseItemQuantity(state, action) {
+            const itemToIncrease = state.cartItems.find(item => item.id === action.payload);
+            if (itemToIncrease) {
+                itemToIncrease.quantity += 1;
+            }
+        },
+        decreaseItemQuantity(state, action) {
+            const itemToDecrease = state.cartItems.find(item => item.id === action.payload);
+            if (itemToDecrease && itemToDecrease.quantity > 1) {
+                itemToDecrease.quantity -= 1;
+            }
+        },
     }
 });
+
+export const {
+    addItemToCart,
+    removeItemFromCart,
+    clearCart,
+    increaseItemQuantity,
+    decreaseItemQuantity,
+} = CartSlice.actions;
+
+export default CartSlice.reducer;
 
 
