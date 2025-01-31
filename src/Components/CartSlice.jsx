d90f144deff36510c5f41004cced2054dbe5865c
imhefizh <hafidzsml@gmail.com>
2025-01-30 23:27:58 -0500

    step 5

--- src/Components/CartSlice.jsx
+++ src/Components/CartSlice.jsx
@@ -1,6 +1,22 @@
+import createSlice from '@reduxjs/toolkit'
 
-const CartSlice = ({
- 
+const initialState = {
+    cartItems: [],
+}
+
+const CartSlice = createSlice({
+    name: 'cart',
+    initialState,
+    reducers: {
+        addItemToCart(state, action) {
+            const existingItem = state.cartItems.find(item => item.id === action.payload.id);
+            if (existingItem) {
+                existingItem.quantity += 1;
+            } else {
+                state.cartItems.push({ ...action.payload, quantity: 1 });
+            }
+        },
+    }
 });
 
 
